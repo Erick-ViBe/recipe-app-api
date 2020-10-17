8d6b887be1c23f9f69db5f3cd3cca929b727a820
Erick-ViBe <erickvb12@gmail.com>
2020-10-16 21:13:05 -0500

    Added endpoint for creating and authenticating users

---
+++ app/user/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from user import views as UserViews
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path(
+        'create/',
+        UserViews.CreateUserAPIView.as_view(),
+        name='create'
+    ),
+
+    path(
+        'token/',
+        UserViews.CreateTokenAPIView.as_view(),
+        name='token'
+    ),
+]
